3896fc3e5b6d35c4fe07d2d20d9f3967d7d2729f
Oscar Newman <newman.oscar@gmail.com>
2013-07-23 01:36:11 -0500

    Fixed a space

--- particular_common/particular/block/BlockTierThreeMachineHousing.java
+++ particular_common/particular/block/BlockTierThreeMachineHousing.java
@@ -8,7 +8,7 @@ public class BlockTierThreeMachineHousing extends BlockParticular {
 	public BlockTierThreeMachineHousing(int id) {
 		super(id, Material.iron);
 		this.setUnlocalizedName(Strings.TIER_THREE_MACHINE_HOUSING_NAME);
-        this.setHardness(3F);
+		this.setHardness(3F);
 	}
 
 }
