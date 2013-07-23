cab407f109e0f2592a2594b28b6398d7f2997df4
Oscar Newman <newman.oscar@gmail.com>
2013-07-22 21:35:06 -0500

    Comments

--- particular_common/particular/Particular.java
+++ particular_common/particular/Particular.java
@@ -9,27 +9,23 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 
-@Mod(modid=Reference.MOD_ID, 
-	 name=Reference.MOD_NAME, 
-	 version=Reference.VERSION)
-
-@NetworkMod(clientSideRequired=true, serverSideRequired=false)
-
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
+@NetworkMod(clientSideRequired=true, serverSideRequired=true)
 public class Particular {
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
-		
+		//Register blocks & items
 	}
 	
 	@EventHandler
 	public void load(FMLInitializationEvent event){
-		
+		//Register events
 	}
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event){
-		
+		//Interact with other mods
 	}
 	
 	
