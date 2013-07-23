7993fb0510ba2d89160206a3064f3b49856a8791
Oscar Newman <newman.oscar@gmail.com>
2013-07-22 20:45:20 -0500

    Init Methods

--- particular_common/particular/Particular.java
+++ particular_common/particular/Particular.java
@@ -1,5 +1,37 @@
 package particular;
 
+import particular.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+
+@Mod(modid=Reference.MOD_ID, 
+	 name=Reference.MOD_NAME, 
+	 version=Reference.VERSION)
+
+@NetworkMod(clientSideRequired=true, serverSideRequired=false)
+
 public class Particular {
 
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void load(FMLInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
+	
+	
+	
 }
---
+++ particular_common/particular/lib/Reference.java
@@ -0,0 +1,14 @@
+package particular.lib;
+
+
+public class Reference {
+
+	public static final boolean DEBUG_MODE = false;
+	
+	public static final String MOD_ID = "Particular";
+	public static final String MOD_NAME = "Particular";
+	public static final String VERSION = "0.0.1";
+			
+			
+	
+}
