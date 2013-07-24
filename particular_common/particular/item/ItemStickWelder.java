f0cec0574009f8dea681fe8d8393c97040c6d050
Oscar Newman <newman.oscar@gmail.com>
2013-07-24 00:54:28 -0500

    Breaking Stick (Not Quite Functional)

--- particular_common/particular/item/ItemStickWelder.java
+++ particular_common/particular/item/ItemStickWelder.java
@@ -2,7 +2,6 @@ package particular.item;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.src.ModLoader;
 import net.minecraft.world.World;
 import particular.lib.Strings;
 
@@ -15,15 +14,22 @@ public class ItemStickWelder extends ItemParticular {
 		this.setUnlocalizedName(Strings.STICK_WELDER_NAME);
 		this.setMaxDamage(10);
 	}
-	
-	 @Override
-	    public boolean onItemUse(ItemStack itemStack, EntityPlayer entityPlayer, World world, int x, int y, int z, int sideHit, float hitVecX, float hitVecY, float hitVecZ) {
 
-	        if (world.isRemote) {
-	        	ModLoader.getMinecraftInstance().thePlayer.addChatMessage("IT WORKS");
-	        	itemStack.damageItem(1, entityPlayer);
-	        	
-	        }
-	        return true;
-	    }
+	@Override
+	public boolean onItemUse(ItemStack itemStack, EntityPlayer entityPlayer, World world, int x, int y, int z, int sideHit, float hitVecX, float hitVecY, float hitVecZ) {
+
+		if(itemStack.getItemDamage() >= 10){
+			itemStack.damageItem(1, entityPlayer);
+			itemStack = new ItemStack(ModItems.emptyStickWelder, 2, 0);
+		}
+		else{
+			itemStack.damageItem(1, entityPlayer);
+		}
+
+
+
+		return true;
+	}
+
+
 }
