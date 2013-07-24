8c74ac1e42e94a3c326ac3d8db62d9552cb5ad31
Oscar Newman <newman.oscar@gmail.com>
2013-07-23 23:33:02 -0500

    Added Scientific Journal

--- particular_common/particular/item/ModItems.java
+++ particular_common/particular/item/ModItems.java
@@ -1,7 +1,6 @@
 package particular.item;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import particular.lib.ItemIds;
 import particular.lib.Strings;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -12,15 +11,19 @@ public class ModItems {
 	//Mod Item Instances
 	public static Item emptyStickWelder;
 	public static Item stickWelder;
+	public static Item scientificJournal;
 	
 	public static void init() {
 		emptyStickWelder = new ItemEmptyStickWelder(ItemIds.EMPTY_STICK_WELDER);
 		stickWelder = new ItemStickWelder(ItemIds.STICK_WELDER);
+		scientificJournal = new ItemScientificJournal(ItemIds.SCIENTIFIC_JOURNAL);
 		
 		GameRegistry.registerItem(emptyStickWelder, Strings.EMPTY_STICK_WELDER_NAME);
 		GameRegistry.registerItem(stickWelder, Strings.STICK_WELDER_NAME);
+		GameRegistry.registerItem(scientificJournal, Strings.SCIENTIFIC_JOURNAL_NAME);
 		
 		LanguageRegistry.addName(emptyStickWelder, "Stick Welder (Empty)");
 		LanguageRegistry.addName(stickWelder, "Stick Welder (Full)");
+		LanguageRegistry.addName(scientificJournal, "Scientific Journal");
 	}
 }
