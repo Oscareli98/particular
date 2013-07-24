16a9510179d804f1959b2f156b023d91a4f423b3
Hunter <hunter@smallworks.com>
2013-07-24 04:16:15 -0500

    Revert "Revert "Ore gen and a start on the hypersonic pistion""
    This reverts commit 74a644dd405d857955439147f914298033264e08.

--- particular_common/particular/Particular.java
+++ particular_common/particular/Particular.java
@@ -6,6 +6,7 @@ import particular.core.proxy.CommonProxy;
 import particular.creativetab.CreativeTabParticular;
 import particular.item.ModItems;
 import particular.lib.Reference;
+import particular.world.EventManager;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -14,6 +15,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 
@@ -30,6 +32,8 @@ public class Particular {
 	public static CreativeTabs tabsParticular = new CreativeTabParticular(
 			CreativeTabs.getNextID(), Reference.MOD_ID);
 
+	EventManager eventmanager = new EventManager();
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
 		//Register blocks & items
@@ -41,6 +45,8 @@ public class Particular {
 	public void load(FMLInitializationEvent event){
 		//Register events
 		LanguageRegistry.instance().addStringLocalization("itemGroup.Particular", "Particular");
+		
+		GameRegistry.registerWorldGenerator(eventmanager);
 	}
 	
 	@EventHandler
