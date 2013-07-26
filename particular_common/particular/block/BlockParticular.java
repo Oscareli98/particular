96cb16f830b57263bc1f56b2300f694062fc0744
Oscar <newman.oscar@gmail.com>
2013-07-26 00:04:22 -0500

    onPlacedBy method in BlockParticular

--- particular_common/particular/block/BlockParticular.java
+++ particular_common/particular/block/BlockParticular.java
@@ -3,6 +3,11 @@ package particular.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import particular.Particular;
 import particular.lib.Reference;
 import cpw.mods.fml.relauncher.Side;
@@ -30,4 +35,31 @@ public class BlockParticular extends Block {
 		return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
 	}
 
+	 @Override
+	    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entityLiving, ItemStack itemStack) {
+
+	        int direction = 0;
+	        int facing = MathHelper.floor_double(entityLiving.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+
+	        if (facing == 0) {
+	            direction = ForgeDirection.NORTH.ordinal();
+	        }
+	        else if (facing == 1) {
+	            direction = ForgeDirection.EAST.ordinal();
+	        }
+	        else if (facing == 2) {
+	            direction = ForgeDirection.SOUTH.ordinal();
+	        }
+	        else if (facing == 3) {
+	            direction = ForgeDirection.WEST.ordinal();
+	        }
+
+	        world.setBlockMetadataWithNotify(x, y, z, direction, 3);
+
+	        if (itemStack.hasDisplayName()) {
+	            //((TileEE) world.getBlockTileEntity(x, y, z)).setCustomName(itemStack.getDisplayName());
+	        }
+
+	        //((TileEE) world.getBlockTileEntity(x, y, z)).setOrientation(direction);
+	    }
 }
