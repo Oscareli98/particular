d3b79e49cd3df315e86f0ab22b23935608be2f25
Hunter <hunter@smallworks.com>
2013-07-25 23:46:15 -0500

    Piston sided textures kind of working

--- particular_common/particular/block/BlockHypersonicPiston.java
+++ particular_common/particular/block/BlockHypersonicPiston.java
@@ -6,9 +6,12 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import particular.lib.Reference;
 import particular.lib.Strings;
 
@@ -21,6 +24,23 @@ public class BlockHypersonicPiston extends BlockParticular {
 
 	}
 
+	public static int determineOrientation(World par0World, int par1, int par2, int par3, EntityLivingBase par4EntityLivingBase) {
+		if (MathHelper.abs((float)par4EntityLivingBase.posX - (float)par1) < 2.0F && MathHelper.abs((float)par4EntityLivingBase.posZ - (float)par3) < 2.0F)
+		{
+			double d0 = par4EntityLivingBase.posY + 1.82D - (double)par4EntityLivingBase.yOffset;
+
+			if (d0 - (double)par2 > 2.0D)
+			{
+				return 1;
+			}
+
+			if ((double)par2 - d0 > 0.0D) {
+				return 0;
+			}
+		}
+		int l = MathHelper.floor_double((double)(par4EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+		return l == 0 ? 2 : (l == 1 ? 5 : (l == 2 ? 3 : (l == 3 ? 4 : 0)));
+	}
 	@SideOnly(Side.CLIENT) 
 	protected Icon bottom;    
 	@SideOnly(Side.CLIENT) 
@@ -50,55 +70,36 @@ public class BlockHypersonicPiston extends BlockParticular {
 	@SideOnly(Side.CLIENT)
 	@Override
 	public Icon getIcon(int side, int meta) {
-		//    	if (meta == 0 && side == 3)
-		//    		return faceIcon;
-		//    	if (meta == 2 && side == 2)
-		//    		return faceIcon;
-		//    	if (meta == 1 && side == 4)
-		//    		return faceIcon;
-		//    	if (meta == 3 && side == 5)
-		//    		return faceIcon;
-		if (side == 0)
+		if (meta == 0 && side == 0)
 			return bottom;
-		if (side == 1)
+		if (meta == 1 && side == 1)
 			return top;
-		if (side == 2)
-			return North;
-		if (side == 3)
+		//		if (side == 2)
+		//			return North;
+		//		if (side == 3)
+		//			return South;
+		//		if (side == 4)
+		//			return West;
+		//		if (side == 5)
+		//			return East;
+		if (meta == 3 && side == 3)
 			return South;
-		if (side == 4)
+		if (meta == 2 && side == 2)
+			return North;
+		if (meta == 4 && side == 4)
 			return West;
-		if (side == 5)
+		if (meta == 5 && side == 5)
 			return East;
 		return blockIcon;
 	}
 
-	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLiving entityliving) {
-		System.out.println("onBlockPlaced is Working");
-		int l = MathHelper.floor_double((double)((entityliving.rotationYaw * 4F) / 360F) + 0.5D) & 3;
 
-		switch (l) {
-			case 0:
-				world.setBlockMetadataWithNotify(x, y, z, 2, 2);
-				System.out.println("case 0" + l);
-				break;
+	public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+	{
+		int l = determineOrientation(par1World, par2, par3, par4, par5EntityLivingBase);
+		par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
 
-			case 1:
-				world.setBlockMetadataWithNotify(x, y, z, 5, 2);
-				System.out.println("case 1" + l);
-				break;
 
-			case 2:
-				world.setBlockMetadataWithNotify(x, y, z, 3, 2);
-				System.out.println("case 2" + l);
-				break;
-
-			case 3:
-				world.setBlockMetadataWithNotify(x, y, z, 4, 2);
-				System.out.println("case 3" + l);
-				break;
-		}
 	}
-
 
 }
