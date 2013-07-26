dd33c4271ee5997e4e9a05a7bd3bfda86693d7de
Hunter <hunter@smallworks.com>
2013-07-26 02:03:52 -0500

    Just some code cleanup

--- particular_common/particular/block/BlockHypersonicPiston.java
+++ particular_common/particular/block/BlockHypersonicPiston.java
@@ -20,21 +20,12 @@ public class BlockHypersonicPiston extends BlockParticular {
 		super(id, Material.rock);
 		this.setUnlocalizedName(Strings.HYPERSONIC_PISTON_NAME);
 		this.setHardness(2F);
-
 	}
 
 	@SideOnly(Side.CLIENT) 
 	protected Icon bottom;    
 	@SideOnly(Side.CLIENT) 
 	protected Icon top;
-//	@SideOnly(Side.CLIENT) 
-//	protected Icon North;
-//	@SideOnly(Side.CLIENT) 
-//	protected Icon South;
-//	@SideOnly(Side.CLIENT)
-//	protected Icon West;
-//	@SideOnly(Side.CLIENT)
-//	protected Icon East;
 
 	@SideOnly(Side.CLIENT)
 	@Override
@@ -43,10 +34,6 @@ public class BlockHypersonicPiston extends BlockParticular {
 		this.blockIcon = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
 		this.bottom = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), "tierOneMachineHousing"));
 		this.top = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), "1" + getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
-//		this.North = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), "2" + getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
-//		this.South = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), "3" + getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
-//		this.West = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), "4" + getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
-//		this.East = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), "5" + getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
 	}
 
 	@SideOnly(Side.CLIENT)
@@ -54,6 +41,7 @@ public class BlockHypersonicPiston extends BlockParticular {
 	public Icon getIcon(int side, int meta) {
 		int k = getOrientation(meta);
 		return k > 5 ? this.top : (side == k ? (!isExtended(meta) && this.minX <= 0.0D && this.minY <= 0.0D && this.minZ <= 0.0D && this.maxX >= 1.0D && this.maxY >= 1.0D && this.maxZ >= 1.0D ? this.top : this.blockIcon) : (side == Facing.oppositeSide[k] ? this.bottom : this.blockIcon));
+		// Keep so we know how to do multi sided blocks, not pistons, which are special in their own way
 		//		if (meta == 0 && side == 0)
 		//			return bottom;
 		//		if (meta == 1 && side == 1)
@@ -77,35 +65,16 @@ public class BlockHypersonicPiston extends BlockParticular {
 		//		return blockIcon;
 	}
 
-	
-	public static int getOrientation(int par0) {
-		return par0 & 7;
-	}
-
-	public static boolean isExtended(int par0) {
-		return (par0 & 8) != 0;
-	}
-    
-    public int getRenderType()
-    {
-        return 16;
-    }
-
-
 	public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack) {
 		int l = determineOrientation(par1World, par2, par3, par4, par5EntityLivingBase);
 		par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
-
-
 	}
-	
+
 	public static int determineOrientation(World par0World, int par1, int par2, int par3, EntityLivingBase par4EntityLivingBase) {
-		if (MathHelper.abs((float)par4EntityLivingBase.posX - (float)par1) < 2.0F && MathHelper.abs((float)par4EntityLivingBase.posZ - (float)par3) < 2.0F)
-		{
+		if (MathHelper.abs((float)par4EntityLivingBase.posX - (float)par1) < 2.0F && MathHelper.abs((float)par4EntityLivingBase.posZ - (float)par3) < 2.0F) {
 			double d0 = par4EntityLivingBase.posY + 1.82D - (double)par4EntityLivingBase.yOffset;
 
-			if (d0 - (double)par2 > 2.0D)
-			{
+			if (d0 - (double)par2 > 2.0D) {
 				return 1;
 			}
 
@@ -116,4 +85,17 @@ public class BlockHypersonicPiston extends BlockParticular {
 		int l = MathHelper.floor_double((double)(par4EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
 		return l == 0 ? 2 : (l == 1 ? 5 : (l == 2 ? 3 : (l == 3 ? 4 : 0)));
 	}
+
+	public static int getOrientation(int par0) {
+		return par0 & 7;
+	}
+
+	public static boolean isExtended(int par0) {
+		return (par0 & 8) != 0;
+	}
+
+	public int getRenderType() {
+		return 16;
+	}
+
 }
