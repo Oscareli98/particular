66efc4d966a486c6e92081dea25943b36ca708a6
Oscar <newman.oscar@gmail.com>
2013-07-27 23:25:05 -0500

    Started TileEntity

---
+++ particular_common/particular/inventory/ContainerSmasherController.java
@@ -0,0 +1,18 @@
+package particular.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+public class ContainerSmasherController extends Container {
+
+	public ContainerSmasherController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean canInteractWith(EntityPlayer entityplayer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- particular_common/particular/tileentity/TileSmasherController.java
+++ particular_common/particular/tileentity/TileSmasherController.java
@@ -1,9 +1,85 @@
 package particular.tileentity;
 
-public class TileSmasherController extends TileParticular {
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 
+public class TileSmasherController extends TileParticular implements IInventory{
+
+	private ItemStack inv[];
+	
 	public TileSmasherController() {
-		// TODO Auto-generated constructor stub
+		inv = new ItemStack[9];
+	}
+
+	@Override
+	public int getSizeInventory() {
+		return inv.length;
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int slot) {
+		return inv[slot];
+	}
+
+	@Override
+	public ItemStack decrStackSize(int slot, int amt) {
+		ItemStack stack = getStackInSlot(slot);
+		return null;
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInventorySlotContents(int i, ItemStack itemstack) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getInvName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isInvNameLocalized() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getInventoryStackLimit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void openChest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeChest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
