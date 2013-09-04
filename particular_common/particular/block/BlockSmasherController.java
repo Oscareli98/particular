728331f9a1bf3a8e3867925aad2a0cc817b9985b
Hunter <hunter@smallworks.com>
2013-09-04 11:50:42 -0500

    Fixed one error

--- particular_common/particular/block/BlockSmasherController.java
+++ particular_common/particular/block/BlockSmasherController.java
@@ -2,6 +2,8 @@ package particular.block;
 
 import java.util.Random;
 
+import cpw.mods.fml.relauncher.SideOnly;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.item.EntityItem;
