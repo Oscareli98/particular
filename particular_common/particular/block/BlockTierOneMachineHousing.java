732a2847403a95e638322701b5a9f3234c6b6445
Hunter <hunter@smallworks.com>
2013-10-25 17:55:30 -0500

    Working on Genning a gradient with java, almost complete

--- particular_common/particular/block/BlockTierOneMachineHousing.java
+++ particular_common/particular/block/BlockTierOneMachineHousing.java
@@ -1,5 +1,14 @@
 package particular.block;
 
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.SwingConstants;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
@@ -15,7 +24,11 @@ import net.minecraft.world.World;
 public class BlockTierOneMachineHousing extends BlockParticular {
 
 	public boolean neighborBlockChange;
-	String blockSurfaces = "";
+	
+	@SideOnly(Side.CLIENT)
+	public boolean a;
+	@SideOnly(Side.CLIENT) 
+	protected Icon front;
 
 	public BlockTierOneMachineHousing(int id) {
 		super(id, Material.iron);
@@ -27,13 +40,13 @@ public class BlockTierOneMachineHousing extends BlockParticular {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IconRegister iconRegister) {
-
-		blockIcon = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+		this.blockIcon = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+		this.front = iconRegister.registerIcon(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
 		func_111022_d(String.format("%s:%s", Reference.MOD_ID.toLowerCase(), getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
 	}
 
 	public Icon getIcon(int side, int meta) {
-		int k = getOrientation(meta);
+		//int k = getOrientation(meta);
 		//		if (meta == 0 && side == 0)
 		//			return bottom;
 		//		if (meta == 1 && side == 1)
@@ -54,33 +67,37 @@ public class BlockTierOneMachineHousing extends BlockParticular {
 		//			return West;
 		//		if (meta == 5 && side == 5)
 		//			return East;
-		if (blockSurfaces == "x") {
-			
-		} else if (blockSurfaces == "y") {
-			
-		} else if (blockSurfaces == "z") {
-			
-		}
-
 		return blockIcon;
-
 	}
 
 	public void determineIcons(World world, int x, int y, int z, int meta) {
-
-		System.out.println("i ran");
-		if (world.getBlockId(x + 1, y, z) == BlockIds.TIER_ONE_MACHINE_HOUSING || world.getBlockId(x - 1, y, z) == BlockIds.TIER_ONE_MACHINE_HOUSING) {
-			blockSurfaces = "x";
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("tierOneMachineHousing2.png"));
+		    for (int i = 0; i != 16; i++) {
+		    	for (int j = 0; j != 16; j++) {
+		    		if (i < 8) {
+		    			if (j < 8) {
+		    				img.setRGB(i, j, img.getRGB(i, j) -(j*3));
+			    		} else {
+			    			img.setRGB(i, j, img.getRGB(i, j) + (j*3));
+			    		}
+		    		} else {
+		    			if (j < 8) {
+		    				img.setRGB(i, j, img.getRGB(i, j) -(j*3));
+			    		} else {
+			    			img.setRGB(i, j, img.getRGB(i, j) + (j*3));
+			    		}
+		    		}
+		    	}
+		    }
+		    File outputfile = new File("tierOneMachineHousing.png");
+		    ImageIO.write(img, "png", outputfile);
+		} catch (IOException e) {
+			img = null;
 		}
-		if (world.getBlockId(x, y + 1, z) == BlockIds.TIER_ONE_MACHINE_HOUSING || world.getBlockId(x, y - 1, z) == BlockIds.TIER_ONE_MACHINE_HOUSING) {
-			blockSurfaces = "y";
-		}
-		if (world.getBlockId(x, y, z + 1) == BlockIds.TIER_ONE_MACHINE_HOUSING || world.getBlockId(x, y, z - 1) == BlockIds.TIER_ONE_MACHINE_HOUSING) {
-			blockSurfaces += "z";
-		}
-		blockSurfaces = "x";
+		
 	}
-
 
 	public void onNeighborBlockChange(World par1World, int x, int y, int z, int meta) {
 		determineIcons(par1World, x, y, z, meta);
